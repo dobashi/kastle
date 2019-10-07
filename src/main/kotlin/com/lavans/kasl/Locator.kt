e36c7a44f679ab634b1a5c1ab5ea40459a17ea75
dobashi <dobashi@lavans.jp>
2019-10-08 01:10:47 +0900

    add testcase for prototype

--- src/main/kotlin/com/lavans/kasl/Locator.kt
+++ src/main/kotlin/com/lavans/kasl/Locator.kt
@@ -1,14 +1,19 @@
 package com.lavans.kasl
 
 import kotlin.reflect.KClass
+import com.lavans.kasl.KClassInfo.Type
+
+typealias Klass = KClass<out Any>
+fun Klass.name(): String = qualifiedName!!
 
 object Locator {
     private val map = mutableMapOf<String, KClassInfo>()
-//    fun get(id: String)  = map.getOrPut(id) { KClassInfo(id) }.getInstance()
+    //    fun get(id: String)  = map.getOrPut(id) { KClassInfo(id) }.getInstance()
     fun <T> get(id: String): T = map.getOrPut(id) { KClassInfo(id) }.getInstance() as T
-    fun <T> get(kclass: KClass<out Any>): T = get(kclass.qualifiedName!!)
-    fun put(id: String, className: String): KClassInfo? = map.put(id, KClassInfo(id, className))
-    fun put(id: String, kclass: KClass<out Any> ): KClassInfo? = map.put(id, KClassInfo(id, kclass))
-    fun put(idClass: KClass<out Any>, kclass: KClass<out Any> ) = put(idClass.qualifiedName!!, kclass)
-    fun put(idClass: KClass<out Any>, className: String) = put(idClass.qualifiedName!!, className)
+    fun <T> get(klass: Klass): T = get(klass.name())
+
+    fun put(id: String, className: String, type: Type = Type.Singleton): KClassInfo? = map.put(id, KClassInfo(id, className, type))
+    fun put(id: String, klass: Klass, type: Type = Type.Singleton): KClassInfo? = map.put(id, KClassInfo(id, klass, type))
+    fun put(idClass: Klass, klass: Klass = idClass, type: Type = Type.Singleton) = put(idClass.name(), klass, type)
+//    fun put(idClass: Klass, className: String = idClass.name(), type: Type = Type.Singleton) = put(idClass.name(), className, type)
 }
--- src/test/kotlin/com/lavans/kasl/LocatorTest.kt
+++ src/test/kotlin/com/lavans/kasl/LocatorTest.kt
@@ -20,4 +20,29 @@ class LocatorTest {
         println(a.say())
         assertTrue(a.say().contains("Yo"))
     }
+
+    @Test
+    fun `Singleton returns same instance`(){
+        val a = Locator.get<A>(A::class)
+        val b = Locator.get<A>(A::class)
+        val c = Locator.get<A>(A::class)
+        val d = Locator.get<A>(A::class)
+        println("$a\n$b\n$c\n$d")
+        assertEquals(a, b)
+        assertEquals(a, c)
+        assertEquals(a, d)
+    }
+
+    @Test
+    fun `Prototype returns different instance`(){
+        Locator.put(A::class, type = KClassInfo.Type.Prototype)
+        val a = Locator.get<A>(A::class)
+        val b = Locator.get<A>(A::class)
+        val c = Locator.get<A>(A::class)
+        val d = Locator.get<A>(A::class)
+        println("$a\n$b\n$c\n$d")
+        assertNotEquals(a, b)
+        assertNotEquals(a, c)
+        assertNotEquals(a, d)
+    }
 }
